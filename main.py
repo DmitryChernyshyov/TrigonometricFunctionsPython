e52d27bac8f9695524e27b5f1f531ab6348867ba
demish <chernyshyov.2000@mail.ru>
2019-03-02 21:59:51 +0700

    import all
    import all

---
+++ main.py
@@ -0,0 +1,9 @@
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import tkinter as Tk
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import matplotlib.animation as animation
+#---------End of imports
